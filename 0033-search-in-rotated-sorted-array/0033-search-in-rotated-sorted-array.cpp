9226aaa835d742497fc1e988b3f8dd0164b52702
Hariom kurmi patel <patelhariomk@gmail.com>
2023-05-04 12:12:00 +0530

    DSA

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,30 +1,31 @@
-class Solution {
+class Solution
+{
 public:
-    int search(vector<int>& nums, int target) {
-        
+    int search(vector<int> &nums, int target)
+    {
+
         int low = 0;
-        int high = nums.size()-1,mid;
-             
-        while(high>=low){
-            
-           mid = low + (high - low)/2;
-            
-            if(nums[mid] == target)
+        int high = nums.size() - 1, mid;
+        while (high >= low)
+        {
+            mid = low + (high - low) / 2;
+            if (nums[mid] == target)
                 return mid;
-            
-            if(nums[mid] >= nums[low]){
-                
-            if(target>=nums[low] && target<=nums[mid])
-                high = mid -1;
-            else
-                low = mid + 1;
-            
+
+            if (nums[mid] >= nums[low])
+            {
+
+                if (target >= nums[low] && target <= nums[mid])
+                    high = mid - 1;
+                else
+                    low = mid + 1;
             }
-            else{
-                if(target>=nums[mid] && target<=nums[high])
+            else
+            {
+                if (target >= nums[mid] && target <= nums[high])
                     low = mid + 1;
                 else
-                    high = mid -1;
+                    high = mid - 1;
             }
         }
         return -1;
