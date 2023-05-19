b035d7d840c7325b68f99d128a9b942b3f910a86
Hariom kurmi patel <patelhariomk@gmail.com>
2023-05-19 12:12:00 +0530

    DSA

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -5,7 +5,7 @@ public:
     {
         int high = nums.size() - 1;
         int low = 0;
-        // hello here
+        // applying binary serach here
         int idx = -1;
         while (high >= low)
         {
