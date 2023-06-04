18345d52b8fbbdc827298d67bafb8af39bfb31e5
Hariom kurmi patel <patelhariomk@gmail.com>
2023-06-04 12:12:00 +0530

    DSA

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,44 +1,43 @@
 class Solution
-    // {
-    public : int
-             binarySearch(vector<int> &nums, int low, int high, int x)
 {
-
-    if (high >= low)
+public:
+    int binarySearch(vector<int> &nums, int low, int high, int x)
     {
-        int mid = low + (high - low) / 2;
 
-        if (nums[mid] == x)
-            return mid;
-        if (nums[mid] < x)
-            return binarySearch(nums, mid + 1, high, x);
-        else
-            return binarySearch(nums, low, mid - 1, x);
+        if (high >= low)
+        {
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] == x)
+                return mid;
+            if (nums[mid] < x)
+                return binarySearch(nums, mid + 1, high, x);
+            else
+                return binarySearch(nums, low, mid - 1, x);
+        }
+        return -1;
     }
-    return -1;
-}
-vector<int> searchRange(vector<int> &nums, int target)
-{
+    vector<int> searchRange(vector<int> &nums, int target)
+    {
 
-    vector<int> res(2, -1);
-    int mid = binarySearch(nums, 0, nums.size() - 1, target);
-    if (mid == -1)
+        vector<int> res(2, -1);
+        int mid = binarySearch(nums, 0, nums.size() - 1, target);
+        if (mid == -1)
+            return res;
+
+        int r_idx = mid;
+        int l_idx = mid;
+
+        while (r_idx + 1 < nums.size() && nums[r_idx + 1] == target)
+            r_idx++;
+
+        while (l_idx - 1 >= 0 && nums[l_idx - 1] == target)
+            l_idx--;
+
+        res.clear();
+        res.push_back(l_idx);
+        res.push_back(r_idx);
+
         return res;
-
-    int r_idx = mid;
-    int l_idx = mid;
-
-    while (r_idx + 1 < nums.size() && nums[r_idx + 1] == target)
-        r_idx++;
-
-    while (l_idx - 1 >= 0 && nums[l_idx - 1] == target)
-        l_idx--;
-
-    res.clear();
-    res.push_back(l_idx);
-    res.push_back(r_idx);
-
-    return res;
-}
-}
-;
+    }
+};
