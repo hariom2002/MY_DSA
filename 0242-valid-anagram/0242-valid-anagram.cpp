f44d31ca28aeab676b1ab7d5c35b31434bad6d66
Hariom kurmi <82286500+hariom2002@users.noreply.github.com>
2023-06-23 06:07:19 +0530

    Time: 4 ms (95.65%), Space: 7.3 MB (41.94%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        
+        if(s.size() != t.size())return false;
+        vector<int> map(26, 0);
+        
+        for(int i=0;i<s.size();i++)
+            map[s[i] - 97]++;
+        
+        for(int i=0;i<s.size();i++)
+            map[t[i] - 97]--;
+        
+        for(int i=0;i<26;i++)
+            if(map[i] != 0)return false;
+        
+        return true;
+        
+    }
+};
