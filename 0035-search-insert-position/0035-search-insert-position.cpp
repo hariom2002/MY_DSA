9353e22f0308fc87e24de4db4afdd73aa64fffc9
Hariom kurmi patel <patelhariomk@gmail.com>
2023-06-29 12:12:00 +0530

    DSA

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -6,7 +6,7 @@ public:
         int high = nums.size() - 1;
         int low = 0;
 
-        int idx = -1;
+        // int idx = -1;
         while (high >= low)
         {
             int mid = low + (high - low) / 2;
