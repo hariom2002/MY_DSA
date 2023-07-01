df78501233c1938075531b346accb90e72ef8f20
Hariom kurmi <82286500+hariom2002@users.noreply.github.com>
2023-07-01 15:54:02 +0530

    Time: 4 ms (55.52%), Space: 7.7 MB (63.89%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -5,20 +5,26 @@ public:
         int n = obstacleGrid.size();
         int m = obstacleGrid[0].size();
         
-        if(obstacleGrid[0][0] == 1)return 0;
+        vector<vector<int>> dp(n, vector<int> (m,0));
         
-        vector<vector<int>> dp(n+1, vector<int> (m+1,-1));
-        return recursion(obstacleGrid ,n-1,m-1,dp );
-    }
-    
-    int recursion(vector<vector<int>> &grid, int i, int j, vector<vector<int>> &dp){
         
-        if(i == 0 && j == 0)return 1;
-        if(i <0 || j < 0)return 0;
-        if(grid[i][j] == 1)return 0;
-        
-        if(dp[i][j] != -1)return dp[i][j];
-       
-        return dp[i][j] =  recursion(grid, i-1, j,dp) + recursion(grid, i, j-1,dp);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                
+                
+                if(obstacleGrid[i][j] == 1)continue;
+                if(i == 0 && j== 0)dp[i][j]=1;
+                
+                else{
+                    int up = 0,left = 0;
+                if(i>0) up =dp[i-1][j];
+                if(j>0) left=dp[i][j-1];
+                    dp[i][j] = up+left;
+                }
+                
+            }
+        }
+        return dp[n-1][m-1];
+        // return 1;
     }
 };
