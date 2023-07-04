115a24bff48eb1bcb35fa79d0bda5c3caed88828
Hariom kurmi <82286500+hariom2002@users.noreply.github.com>
2023-07-05 01:15:50 +0530

    Time: 7 ms (93.75%), Space: 8.9 MB (74.54%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,31 +1,23 @@
 class Solution {
 public:
     int minDistance(string word1, string word2) {
-        
-        int n = word1.length();
-        int m = word2.length();
-        
-        vector<vector<int>> dp(n+1, vector<int> (m+1, -1));
-        
-        return recursion(word1, word2, n-1, m-1, dp);
-    }
-    
-    int recursion(string &str1, string &str2, int i, int j,vector<vector<int>> &dp){
-        
-        if(j <0)return i+1;
-        if(i < 0)return j+1;
-        
-        if(dp[i][j] != -1)return dp[i][j];
-        
-        if(str1[i] == str2[j]){
-            return dp[i][j] = ( 0 + recursion(str1, str2, i-1, j-1, dp));
+        int m = word1.size(), n = word2.size();
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
+        for (int i = 1; i <= m; i++) {
+            dp[i][0] = i;
         }
-        else{
-            int insert = recursion(str1, str2, i, j-1, dp) ;
-            int remove = recursion(str1, str2, i-1, j, dp) ;
-            int replace = recursion(str1, str2, i-1, j-1, dp) ;
-            
-            return  dp[i][j] = 1+  min(insert, min(remove, replace));
+        for (int j = 1; j <= n; j++) {
+            dp[0][j] = j;
         }
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (word1[i - 1] == word2[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = min(dp[i - 1][j - 1], min(dp[i][j - 1], dp[i - 1][j])) + 1;
+                }
+            }
+        }
+        return dp[m][n];
     }
 };
