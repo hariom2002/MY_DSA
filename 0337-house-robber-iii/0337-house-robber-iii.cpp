6228b6fe44cae225c5adb3af8be5805b2883d4ab
Hariom kurmi <82286500+hariom2002@users.noreply.github.com>
2023-07-09 01:37:30 +0530

    Time: 13 ms (85.61%), Space: 22.2 MB (84.74%) - LeetHub

--- 0337-house-robber-iii/0337-house-robber-iii.cpp
+++ 0337-house-robber-iii/0337-house-robber-iii.cpp
@@ -9,21 +9,25 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution {
+class Solution
+{
 public:
-    unordered_map<TreeNode*, int> map;
-    int rob(TreeNode* root) {
-        
-        if(root == NULL)return 0;
-        
-        if(map.find(root)!=map.end())return map[root];
-        
-        int exc = rob(root->left) + rob(root->right);
-        int inc = 0;
-        if(root->left) inc+=rob(root->left->left) + rob(root->left->right);
-        if(root->right) inc+=rob(root->right->left) + rob(root->right->right);
-        
-        return map[root] = max(exc, root->val+ inc);
-        
+    unordered_map<TreeNode *, int> mp;
+    int rob(TreeNode *root)
+    {
+        if (root == NULL)
+            return 0;
+        auto ans = dfs(root);
+        return max(ans.first, ans.second);
+    }
+    pair<int, int> dfs(TreeNode *root)
+    {
+        if (root == NULL)
+            return {0, 0};
+        auto [leftnotrob, leftrob] = dfs(root->left);
+        auto [rightnotrob, rightrob] = dfs(root->right);
+        int notrob = max(leftnotrob, leftrob) + max(rightnotrob, rightrob);
+        int rob = root->val + leftnotrob + rightnotrob;
+        return {notrob, rob};
     }
 };
