8cf7305e4fd4f6d5dc404f2797c755648062c219
Hariom kurmi <82286500+hariom2002@users.noreply.github.com>
2023-07-16 01:11:11 +0530

    Time: 95 ms (24.33%), Space: 48.5 MB (40.44%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        
+        int rechable = 0;
+        
+        for(int i=0;i<nums.size();i++){
+            if(i>rechable)return false;
+            rechable = max(rechable, i+nums[i]);
+        }
+        return true;
+    }
+};
