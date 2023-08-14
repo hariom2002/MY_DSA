016f37ba32f80b88a304536534c0889831397527
Hariom kurmi <82286500+hariom2002@users.noreply.github.com>
2023-08-14 14:02:00 +0530

    Time: 1971 ms (71.89%), Space: 0B (100.00%) - LeetHub

--- 0577-employee-bonus/0577-employee-bonus.sql
+++ 0577-employee-bonus/0577-employee-bonus.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
 
 select e.name as name, b.bonus as bonus
-from Employee e left join Bonus b on e.empId = b.empId
+from Employee e left join Bonus b using(empId)
 WHERE COALESCE(b.bonus, 0) < 1000; 
