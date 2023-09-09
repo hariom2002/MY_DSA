9b244d7784e0433bb02a0adb8ff9eb2a01e7cbd1
Hariom kurmi <82286500+hariom2002@users.noreply.github.com>
2023-09-09 11:48:22 +0530

    Time: 55 ms (70.08%), Space: 23.5 MB (89.87%) - LeetHub

--- 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.cpp
+++ 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.cpp
@@ -7,7 +7,7 @@ public:
         int m = A[0].size();
         
         for(int i=0;i<n;i++){
-            for(int j = 0;j<m;j++){
+            for(int j = 0;j<m; res += A[i][j++]){
                 
                 if(A[i][j] && i && j){
                     A[i][j] +=min({A[i - 1][j - 1], A[i - 1][j], A[i][j - 1]});
@@ -15,11 +15,11 @@ public:
             }
         }
         
-        for(int i=0;i<n;i++){
-            for(int j = 0;j<m;j++){
-                res+=A[i][j];
-            }
-        }
+        // for(int i=0;i<n;i++){
+        //     for(int j = 0;j<m;j++){
+        //         res+=A[i][j];
+        //     }
+        // }
         return res;
     }
 };
